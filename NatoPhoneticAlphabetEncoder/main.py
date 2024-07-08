e80b1490ad9a177afa6e2e0a50e1fa83db203270
KevinAdes <kra18@hampshire.edu>
2024-07-08 08:41:16 -0400

    Project
    Very simple project for understanding list comprehension

---
+++ NatoPhoneticAlphabetEncoder/main.py
@@ -0,0 +1,9 @@
+﻿import pandas
+
+NPA_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+NPA_dict = {row.letter: row.code for (index, row) in NPA_csv.iterrows()}
+
+in_word = input("Enter a word: ").upper()
+out_codes = [NPA_dict[letter] for letter in in_word]
+print(out_codes)
